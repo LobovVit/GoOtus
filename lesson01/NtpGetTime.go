4f487e801dd5102c477bccbcfd364306495b10fd
LobovVit <lobov.vitaliy@gmail.com>
2020-05-23 20:41:43 +0300

    first commit

---
+++ lesson01/NtpGetTime.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+)
+
+func main() {
+	time, err := ntp.Time("time.nist.gov")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(time)
+	}
+	fmt.Println("Finish")
+}
