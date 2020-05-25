878472170e754eb3eaf2b1eca53fe24209655662
LobovVit <lobov.vitaliy@gmail.com>
2020-05-25 20:55:35 +0300

    lesson 03

--- lesson03/itoa_test.go
+++ lesson03/itoa_test.go
@@ -9,4 +9,5 @@ func TestIota(t *testing.T) {
 	assert.Equal(t, itoa(12313311), "12313311", "Хрень 12313311")
 	assert.Equal(t, itoa(231312312312313), "231312312312313", "Хрень 231312312312313")
 	assert.Equal(t, itoa(-1111), "-1111", "Хрень -11111")
+	assert.Equal(t, itoa(0), "0", "Хрень -11111")
 }
