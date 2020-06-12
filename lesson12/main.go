d12910e991140aeaf6b70a09013e88cd2d8909d9
LobovVit <lobov.vitaliy@gmail.com>
2020-06-12 22:31:01 +0300

    lesson 12

--- lesson12/main.go
+++ lesson12/main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+	flag "github.com/spf13/pflag"
 	"lesson12/pkg"
+	"log"
 )
 
+var firstParam string
+var secondParam string
+var thirdParam []string
+
 func init() {
-	flag.StringVar()
+	flag.StringVar(&firstParam, "d", "", "Scan dir")
+	flag.StringVar(&secondParam, "c", "", "Command")
+	flag.StringSliceVar(&thirdParam, "p", nil, "Params (delimiter \",\")")
+
 }
 
 func main() {
-	fmt.Printf("переменная %T\n", 1000)
-	pkg.ReadDir()
-	pkg.RunCmd()
+	flag.Parse()
+	//fmt.Printf("firstParam type=%T ; firstParam=%v\n", firstParam,firstParam)
+	//fmt.Printf("secondParam type=%T ; secondParam=%v\n", secondParam,secondParam)
+	env, err := pkg.ReadDir(firstParam)
+	if err != nil {
+		log.Fatal(err)
+	}
+	pkg.RunCmd(secondParam, thirdParam, env)
 }
--- lesson12/pkg/utils.go
+++ lesson12/pkg/utils.go
@@ -1,9 +1,50 @@
 package pkg
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
 func ReadDir(dir string) (map[string]string, error) {
-	return nil, nil
+
+	var envMap = make(map[string]string)
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, f := range files {
+		if f.IsDir() {
+			fmt.Printf("%v Это каталог \n", f.Name())
+		} else {
+			fileContent, err := ioutil.ReadFile(filepath.Join(dir, f.Name()))
+			if err != nil {
+				return nil, err
+			}
+			envMap[f.Name()] = string(fileContent)
+		}
+	}
+	//fmt.Printf("%v\n", envMap)
+
+	return envMap, nil
 }
 
-func RunCmd(cmd []string, env map[string]string) int {
+func RunCmd(cmd string, paramCmd []string, env map[string]string) int {
 
+	for param, _ := range env {
+		os.Setenv(param, env[param])
+	}
+
+	c1 := exec.Command(cmd, paramCmd...)
+	//c1.Env = append(os.Environ())
+	//c1.Start()
+	fmt.Println(c1.Env)
+	out, _ := c1.Output()
+	fmt.Printf("%v", string(out))
+	//c1.Wait()
+	return 0
 }
